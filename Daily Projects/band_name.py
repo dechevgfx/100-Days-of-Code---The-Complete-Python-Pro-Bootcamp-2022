8c52c4fd19a46fdac267b443c03775fa01d002b1
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-04-11 20:55:49 +0300

    Update band_name.py

--- Daily Projects/band_name.py
+++ Daily Projects/band_name.py
@@ -1,3 +1,14 @@
+#Tasks:
+
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+
 print("Welcome to Brand Name Generator")
 city = input("What is the Name your City?\n")
 pet = input("What is the Name of your Pet?\n")
