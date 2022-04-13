cc71a695eaf551c3c38e91a62c9e0934a0f27e03
dechevgfx <101424579+dechevgfx@users.noreply.github.com>
2022-04-13 21:22:02 +0300

    Update tip_calculator.py

--- Daily Projects/tip_calculator.py
+++ Daily Projects/tip_calculator.py
@@ -1,3 +1,10 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+
+#Format the result to 2 decimal places = 33.60
+
+
 print("Welcome to the tip calculator!")
 bill = float(input("What is the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
